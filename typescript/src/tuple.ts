7d81b44f52ebc735f5ab5fbdae73bd7debafd58d
Mohammad Hammad <hammad.scripted@gmail.com>
2026-07-25 16:36:58 +0530

    tuples and enums

--- typescript/src/tuple.ts
+++ typescript/src/tuple.ts
@@ -1,7 +1,7 @@
 //? In TypeScript, a tuple is a typed array with a pre-defined length and specific types for each index.
 let person: [string, number, boolean] = ['shakeandbake', 30, true];
 
-let date:readonly [month:number, day:number, year:number] = [1, 2, 2023];
+let date: readonly [month: number, day: number, year: number] = [1, 2, 2023];
 
 console.log(date);
 
@@ -12,3 +12,24 @@ console.log(date);
 // date.push(10);
 
 console.log(date);
+
+enum Server {
+  OFFLINE,
+  ONLINE,
+  PENDING,
+}
+
+let status: [string, Server] = ['offline', Server.OFFLINE];
+
+console.log(status);
+
+enum Direction{
+    North=1,
+    South,
+    East,
+    West    
+}
+
+
+const dir:Direction=Direction.North;
+console.log(dir);
